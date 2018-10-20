05479c57239cbc0cabc8ce647f610a1036862389
levantAJ <sirlevantai@gmail.com>
2018-10-20 18:00:41 +0700

    Install CocoaPods

---
+++ Pods/Target Support Files/Pods-POIsTests/Pods-POIsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_POIsTests : NSObject
+@end
+@implementation PodsDummy_Pods_POIsTests
+@end
---
+++ Pods/Target Support Files/Pods-POIsUITests/Pods-POIsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_POIsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_POIsUITests
+@end
