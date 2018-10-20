0719a2d3c7ff397405fcaf365713a8f0b3e54b77
levantAJ <sirlevantai@gmail.com>
2018-10-20 17:58:36 +0700

    Init project

---
+++ POIs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  POIs
+//
+//  Created by levantAJ on 20/10/18.
+//  Copyright © 2018 levantAJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
